0664d0758619ceb9cc4663d6ba13aae18d0396fc
athan <odys1234@gmail.com>
2019-06-03 16:41:31 +0200

    Initial Ductoris singlton declaration

--- Ductoris/Ductoris.h
+++ Ductoris/Ductoris.h
@@ -1,11 +1,23 @@
 #ifndef DUCTORIS_H
 #define DUCTORIS_H
+#include <QObject>
 
-
-class Ductoris
+class Ductoris : public QObject
 {
+    Q_OBJECT
 public:
+    ~Ductoris();
+
+public slots:
+    void onUnitMove(int x, int y);
+    void onSelectUnit();
+    void onUnitAttack();
+    void onNewUnitCreate();
+    void onUnitAttacked();
+private:
     Ductoris();
+    Ductoris(const Ductoris &other) = delete;
+    Ductoris& operator=(const Ductoris &other) = delete;
 };
 
 #endif // DUCTORIS_H
